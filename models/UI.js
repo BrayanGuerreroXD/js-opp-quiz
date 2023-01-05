38e8d340e4b935503ef967328691a0c269be358e
BrayanGuerreroXD <bg7047653@gmail.com>
2023-01-04 23:12:23 -0500

    Update ui.js file

--- models/UI.js
+++ models/UI.js
@@ -10,4 +10,20 @@ export class UI{
         const questionTitle = document.getElementById('question')
         questionTitle.innerHTML = text
     }
+
+    /**
+     * 
+     * @param {string[]} choices the choices of the question
+     */
+
+    showChoices(choices) {
+        const choicesContainer =  document.getElementById('choices')
+
+        choices.map(choice => {
+            const button = document.createElement('button')
+            button.innerText = choice
+            button.className = 'button'
+            choicesContainer.append(button)
+        })
+    }
 }
