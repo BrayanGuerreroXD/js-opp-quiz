c07e7e133b3baeb66abb69288c879cc9263a262b
BrayanGuerreroXD <bg7047653@gmail.com>
2023-01-04 21:22:01 -0500

    Create data.js

---
+++ data/data.js
@@ -0,0 +1,17 @@
+export const data = [
+    {
+        question: 'Cual de las siguientes opciones no es un editor de código?',
+        choices: ['vim', 'vscode', 'emacs', 'word'],
+        answer: 'word'
+    },
+    {
+        question: 'Qué lenguaje no es orientado a objetos?',
+        choices: ['Java', 'Haskell', 'C++', 'Python'],
+        answer: 'Haskell'
+    },
+    {
+        question: 'Qué lenguaje no sirve para estilizar sitios web?',
+        choices: ['stylus', 'less', 'sass', 'PostCSS', 'NextCSS'],
+        answer: 'PostCSS'
+    }
+]
